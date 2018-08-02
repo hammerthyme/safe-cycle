778764e3fb6517702ec47f2546a20f2318c23528
Samer Bibi <bibi.sam@gmail.com>
2018-08-02 11:13:02 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./public"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  }
+};
