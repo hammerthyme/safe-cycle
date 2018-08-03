d21d8d197f5850510d23d2a0fe7601f72b9d0ad6
Samer Bibi <bibi.sam@gmail.com>
2018-08-03 16:58:20 -0400

    receive data back from accidents api

--- app/components/RenderMap.js
+++ app/components/RenderMap.js
@@ -1,15 +1,48 @@
 import React from "react";
 import { GoogleMap, DirectionsRenderer } from "react-google-maps";
+import axios from "axios";
+
+const MapLatLngDirections = coordsArr => {
+  let mappedLatLng = coordsArr.map(latlng => {
+    let [lat, lng] = [latlng.lat(), latlng.lng()];
+    return [lat, lng];
+  });
+  return mappedLatLng;
+};
+
+const AccidentData = latlngArr => {
+  const apiURL = "https://data.cityofnewyork.us/resource/qiz3-axqb.json?";
+  const appToken = "&$$app_token=3Ar5xAFNG5NV39cgZQlF9cZ6y";
+  let dataStore = [];
+
+  latlngArr.forEach(async latlng => {
+    let lat, lng;
+    [lat, lng] = [latlng[0], latlng[1]];
+    const latlngParam = `$where=within_circle(location,${lat},${lng},10)`;
+    const url = `${apiURL}${latlngParam}${appToken}`;
+    let res = await axios.get(url);
+    let accidentData = res.data;
+    let currDataMap = new Map();
+    currDataMap.set([lat, lng], accidentData);
+    dataStore.push(currDataMap);
+  });
+  return dataStore;
+};
 
 const RenderMap = props => {
+  let latlngArr;
+
+  if (props.directions) {
+    latlngArr = MapLatLngDirections(props.directions.routes[0].overview_path);
+    let accidents = AccidentData(latlngArr);
+    console.log("ACCIDENT DATA", accidents);
+  }
+
   const center = {
     lat: 40.7128, //NYC coordinates
     lng: -74.006
   };
   const zoom = 9;
-  props.directions && console.log(props.directions);
-  //accessing lat/long of each step
-  //console.log(props.directions.routes[0].legs[0].steps[0].start_location.lat());
   return (
     <div>
       <GoogleMap defaultCenter={center} defaultZoom={zoom}>
