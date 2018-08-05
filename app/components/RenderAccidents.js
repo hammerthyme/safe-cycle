6cd9eebd865a6eafc31da2084ab4ff345256d6c9
Samer Bibi <bibi.sam@gmail.com>
2018-08-05 15:30:55 -0400

    add dynamic accident data

--- app/components/RenderAccidents.js
+++ app/components/RenderAccidents.js
@@ -6,14 +6,48 @@ class RenderAccidents extends Component {
     super();
     this.state = {
       isOpen: false,
-      showIdx: -1
+      showIdx: -1,
+      cyclistsInjured: 0,
+      cyclistsKilled: 0,
+      percentageTotal: 0
     };
   }
-  handleHover = idx => {
-    this.setState({ showIdx: idx, isOpen: !this.state.isOpen });
+
+  handleClick = idx => {
+    const [cyclistsInjured, cyclistsKilled, percentageTotal] = this.displayData(
+      this.props.accidents[idx]
+    );
+    this.setState({
+      showIdx: idx,
+      isOpen: !this.state.isOpen,
+      cyclistsInjured,
+      cyclistsKilled,
+      percentageTotal
+    });
   };
   handleClose = () => {
-    this.setState({ showIdx: -1, isOpen: !this.state.isOpen });
+    this.setState({
+      showIdx: -1,
+      isOpen: !this.state.isOpen,
+      cyclistsInjured: 0,
+      cyclistsKilled: 0,
+      percentageTotal: 0
+    });
+  };
+  displayData = accidentMap => {
+    let cyclistsInjured = 0;
+    let cyclistsKilled = 0;
+    const latlngAccidentsArr = accidentMap.values().next().value;
+    latlngAccidentsArr.forEach(accident => {
+      cyclistsInjured += Number(accident.number_of_cyclist_injured);
+      cyclistsKilled += Number(accident.number_of_cyclist_killed);
+    });
+    const percentageTotal = (
+      (cyclistsInjured + cyclistsKilled) /
+      latlngAccidentsArr.length
+    ).toLocaleString(undefined, { style: "percent", minimumFractionDigits: 0 });
+
+    return [cyclistsInjured, cyclistsKilled, percentageTotal];
   };
   render() {
     return this.props.accidents.map((accidentMap, idx) => {
@@ -28,7 +62,7 @@ class RenderAccidents extends Component {
               strokeWeight: 0.5,
               fillColor: "red"
             }}
-            onMouseDown={() => this.handleHover(idx)}
+            onMouseDown={() => this.handleClick(idx)}
           />
           {this.state.isOpen &&
             this.state.showIdx === idx && (
@@ -36,7 +70,20 @@ class RenderAccidents extends Component {
                 options={{ position: position }}
                 onCloseClick={this.handleClose}
               >
-                <span>test</span>
+                <div>
+                  <div>
+                    <strong>Cyclists injured: </strong>{" "}
+                    {this.state.cyclistsInjured}
+                  </div>
+                  <div>
+                    <strong>Cyclists killed: </strong>{" "}
+                    {this.state.cyclistsKilled}
+                  </div>
+                  <div>
+                    <strong>Percent total accidents: </strong>{" "}
+                    {this.state.percentageTotal}
+                  </div>
+                </div>
               </InfoWindow>
             )}
         </div>
