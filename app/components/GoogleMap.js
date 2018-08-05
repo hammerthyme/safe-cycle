4349662328b0160d3460d964848711392ba261ff
Samer Bibi <bibi.sam@gmail.com>
2018-08-04 20:59:47 -0400

    handlesubmit renders accident data but not directions path

--- app/components/GoogleMap.js
+++ app/components/GoogleMap.js
@@ -1,54 +1,12 @@
 import React from "react";
-import { compose, withProps, lifecycle } from "recompose";
+// import { compose, withProps, lifecycle } from "recompose";
 import { withScriptjs, withGoogleMap } from "react-google-maps";
-import RenderMap from "./RenderMap";
-// HELPER FUNCTIONS //
-import fetchAccidentData from "../functions/fetchAccidentData";
-import mapLatLngDirections from "../functions/mapLatLngDirections";
+import RenderMapWrapper from "./RenderMap";
 
-const MyMap = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDr3cIycd9ql4MFBqYfOb80LcZSzFLmDVo&v=3.exp",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `95vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap,
-  lifecycle({
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
-      DirectionsService.route(
-        {
-          origin: new google.maps.LatLng(40.681181, -73.946858),
-          destination: new google.maps.LatLng(40.705076, -74.00916),
-          travelMode: "BICYCLING" //was google.maps.TravelMode.BYCYCLING
-        },
-        //upon retrieving directions, invokes callback passing in DirectionsResult and DirectionsStatus
-        async (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
-            const latlngArr = mapLatLngDirections(
-              result.routes[0].overview_path
-            ); //set of coordinates for entire directions route
-            const accidents = await fetchAccidentData(latlngArr);
-            console.log("ACCIDENTS", accidents);
+const GenerateGoogleMap = withScriptjs(
+  withGoogleMap(() => <RenderMapWrapper />)
+);
 
-            this.setState({
-              directions: result,
-              accidents: accidents
-            });
-          } else {
-            console.error("error fetching directions");
-          }
-        }
-      );
-    }
-  })
-)(props => {
-  return RenderMap(props);
-});
-
-export default MyMap;
+export default GenerateGoogleMap;
 
 // style = {{ height: "100vh", width: "100%" }}
--- app/components/RenderMap.js
+++ app/components/RenderMap.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { GoogleMap, DirectionsRenderer } from "react-google-maps";
 import RenderAccidents from "./RenderAccidents";
+import { connect } from "react-redux";
 
-const RenderMap = props => {
-  const center = {
-    lat: 40.7128, //NYC coordinates
-    lng: -74.006
-  };
-  const zoom = 9;
-  return (
-    <div>
-      <GoogleMap defaultCenter={center} defaultZoom={zoom}>
-        {props.directions && (
-          <DirectionsRenderer directions={props.directions} />
-        )}
-        {props.accidents ? (
-          <RenderAccidents accidents={props.accidents} />
-        ) : (
-          <h1>loading</h1>
-        )}
-      </GoogleMap>
-    </div>
-  );
-};
+class RenderMapWrapper extends Component {
+  render() {
+    const center = {
+      lat: 40.7128, //NYC coordinates
+      lng: -74.006
+    };
+    const zoom = 9;
+    console.log(this.props);
+    return (
+      <div>
+        <GoogleMap defaultCenter={center} defaultZoom={zoom}>
+          {this.props.directions.length && (
+            <DirectionsRenderer directions={this.props.directions} />
+          )}
+          {this.props.accidents.length ? (
+            <RenderAccidents accidents={this.props.accidents} />
+          ) : (
+            <h1>loading</h1>
+          )}
+        </GoogleMap>
+      </div>
+    );
+  }
+}
 
-export default RenderMap;
+const mapState = state => ({
+  directions: state.directions,
+  accidents: state.accidents
+});
+
+export default connect(mapState)(RenderMapWrapper);
