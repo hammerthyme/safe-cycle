6731639aee3a63af822624fb1cce584036513fdf
Samer Bibi <bibi.sam@gmail.com>
2018-08-05 13:05:17 -0400

    add latlng dummy data hover

--- app/functions/mapLatLngDirections.js
+++ app/functions/mapLatLngDirections.js
@@ -1,9 +1,9 @@
 const MapLatLngDirections = coordsArr => {
-  let mappedLatLng = coordsArr.map(latlng => {
+  console.log("initial coords arr", coordsArr);
+  return coordsArr.map(latlng => {
     let [lat, lng] = [latlng.lat(), latlng.lng()];
     return [lat, lng];
   });
-  return mappedLatLng;
 };
 
 export default MapLatLngDirections;
